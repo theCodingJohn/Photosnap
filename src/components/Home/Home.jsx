de0f35e284a81735dfb091a977b6379b35ab3f18
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-18 14:44:56 +0800

    complete hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 function Home() {
   return (
     <main>
-      <section className="hero">
-        <div className="hero-image"></div>
-        <div className="container hero-desc-wrapper">
+      <section className="hero hero-1">
+        <div className="hero-image hero-image-1"></div>
+        <div className="container hero-desc-wrapper hero-desc-wrapper-1">
           <h1>CREATE AND<br />SHARE YOUR<br />PHOTO STORIES.</h1>
           <p>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>
           <a href="/">GET AN INVITE</a>
         </div>
       </section>
-      <section>
-        <div className="hero-image"></div>
-          <div className="container hero-desc-wrapper">
-            <h1>BEAUTIFUL<br />STORIES EVERY<br />TIME</h1>
+      <section className="hero hero-2">
+        <div className="hero-image hero-image-2"></div>
+          <div className="container hero-desc-wrapper hero-desc-wrapper-2">
+            <h2>BEAUTIFUL<br />STORIES<br />EVERY TIME</h2>
            <p>We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.</p>
             <a href="/">VIEW THE STORIES</a>
         </div>
       </section>
-      <section>
-        <div className="hero-image"></div>
-          <div className="container hero-desc-wrapper">
-            <h1>DESIGN FOR<br />EVERYONE</h1>
+      <section className="hero hero-3">
+        <div className="hero-image hero-image-3"></div>
+          <div className="container hero-desc-wrapper hero-desc-wrapper-3">
+            <h2>DESIGN FOR<br />EVERYONE</h2>
             <p>Photosnap can help you create stories that resonate with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it.</p>
             <a href="/">VIEW THE STORIES</a>
         </div>
