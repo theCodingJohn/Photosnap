285b1c7a4a0e7e7391b5e0afaae71e4a2018b3aa
John Carlo Cunanan <codingjohn@outlook.com>
2020-12-23 23:47:30 +0800

    add date on card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,9 +4,10 @@ function Card({ card }) {
   return (
     <div className={`card ${card.page}-card-${card.id}`}>
       <div className="desc-wrapper">
+        {card.date && <p className="card-date">{card.date}</p>}
         <h3>{card.name}</h3>
         <p>By {card.author}</p>
-        <p className="line"></p>
+        <span style={{display: 'inline-block'}} className="line"></span>
         <div className="card-button-wrapper">
           <a className="arrow" href="/">
             READ STORY
